9b3a9b98868f6aebf82be957468e41c3ac82001e
xuhao <xh416445194@163.com>
2020-12-04 18:52:51 +0800

    修改

---
+++ apitest/tests/job_api/test_job_api_login.py
@@ -0,0 +1,25 @@
+# -*- codeing = utf-8
+
+
+import unittest
+#
+# from apitest.common.configHttp import ConfigHttp
+# from apitest.common.log import MyLog
+# from apitest.params.params import TestApijobLogin
+
+
+class TestJobApiLogin(unittest.TestCase):
+
+    # def setUp(self) -> None:
+    #     self.mylog = MyLog.get_log()
+
+    def test_job_login(self):
+        # url,data,method = TestApijobLogin.urls[0],TestApijobLogin.datas[0],TestApijobLogin.methods[0]
+        #
+        # res = ConfigHttp(url=url,data=data,method=method).request().json()
+        #
+        # self.mylog.debug("---test_job_login---,响应结果为{}".format(res))
+        #
+        # self.assertEqual("success",res["status"],"登录失败")
+
+        self.assertFalse(0)
